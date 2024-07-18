e36a96556434f99f2d6c1dd89e6ebb82392b15f7
namirinz <nathmcfc31115@gmail.com>
2024-07-18 20:16:14 +0700

    Add tavily search tool

---
+++ packages/components/nodes/tools/TavilySearch/TavilySearch.ts
@@ -0,0 +1,30 @@
+import { SearchApi } from '@langchain/community/tools/searchapi'
+import { ICommonObject, INode, INodeData, INodeParams } from '../../../src/Interface'
+import { getBaseClasses, getCredentialData, getCredentialParam } from '../../../src/utils'
+
+class TavilySearch implements INode {
+    label: string
+    name: string
+    version: number
+    description: string
+    type: string
+    icon: string
+    category: string
+    baseClasses: string[]
+    credential: INodeParams
+    inputs: INodeParams[]
+
+    constructor() {
+        this.label = 'Tavily Search'
+        this.name = 'tavilySearch'
+        this.version = 1.0
+        this.type = 'tools'
+        this.icon = 'tavilySearch.jpeg'
+        this.category = 'Tools'
+        this.description = "Tavily's Search API is a search engine built specifically for AI agents"
+        this.inputs = []
+        this.baseClasses = [this.type, ...getBaseClasses(SearchApi)]
+    }
+}
+
+module.exports = { nodeClass: TavilySearch }
